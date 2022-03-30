27e65a457e1646c86524727270efef08b1bf2c4d
prathamesh sawant <prathameshpgs@gmail.com>
2022-03-31 00:00:28 +0530

    Removed a from login description

--- src/pages/LoginPage/LoginPage.component.jsx
+++ src/pages/LoginPage/LoginPage.component.jsx
@@ -16,7 +16,7 @@ function LoginPage() {
             <CallToActionLogoOne src={CTALogoOne.default}/>
             <SignUp>GET ALL THERE</SignUp>
             <Description>
-                Get a Premier Access to Raya and the Last Dragon for an additional fee with a Disney+ subscription. As of 03/26/21, the price of Disney+ and The Disney Bundle will increase by $1.
+                Get Premier Access to Raya and the Last Dragon for an additional fee with a Disney+ subscription. As of 03/26/21, the price of Disney+ and The Disney Bundle will increase by $1.
             </Description>
             <CallToActionLogoTwo src={CTALogoTwo}/>
         </CallToAction>
